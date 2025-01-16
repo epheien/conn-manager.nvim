ad50ebb5c9f6b2bee8a49b763a6e7b03b38a7958
eph <eph@MacBook-Pro.local>
2025-01-16 20:52:33 +0800

    feat: improve root node render

--- lua/conn-manager/render.lua
+++ lua/conn-manager/render.lua
@@ -1,7 +1,14 @@
 local M = {}
 
+---@param bufnr integer
+---@param tree Node
+---@return Node[]
 function M.render(bufnr, tree)
-  local lines, line_to_node = tree:render()
+  local lines, line_to_node = tree:render(-1)
+  -- root 节点需要特殊处理显示效果
+  if #lines > 0 then
+    lines[1] = 'Connections'
+  end
   vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
   return line_to_node
 end
